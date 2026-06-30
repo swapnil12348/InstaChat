af30930095725652618712c6b897237752fabca1
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-06-30 10:28:39 +0530

    created a basic expo app

---
+++ client/app/(auth)/_layout.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Auth_layout() {
+  return (
+    <View>
+      <Text>Auth_layout</Text>
+    </View>
+  )
+}
---
+++ client/app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function tabs_layout() {
+  return (
+    <View>
+      <Text>tabs_layout</Text>
+    </View>
+  )
+}
