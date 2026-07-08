57d36acf03dafcec195456aa24c87fe3e6fc0b36
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-07-08 19:09:44 +0530

    tabs

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,10 +1,31 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router';
+import { Colors } from '@/constants/Colors';
 
 export default function tabs_layout() {
   return (
-    <View>
-      <Text>tabs_layout</Text>
-    </View>
+    <Tabs screenOptions={{
+      headerShown: false,
+      tabBarActiveTintColor: Colors.primary,
+      tabBarInactiveTintColor: Colors.onSurfaceVariant,
+      tabBarStyle:{
+        backgroundColor: Colors.primary,
+        borderTopColor:Colors.surfaceHigh,
+        borderTopWidth: 1,
+        height: 80,
+        paddingBottom: 12,
+        paddingTop: 8,
+
+      },
+      tabBarLabelStyle:{
+        fontSize: 14,
+        fontWeight: "600",
+      }
+    }}>
+      <Tabs.Screen name='index'/>
+      <Tabs.Screen name='search'/>
+      <Tabs.Screen name='profile'/>
+    </Tabs>
   )
 }
