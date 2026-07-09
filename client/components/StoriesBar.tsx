8877325c1b912eba777ffec7f4216ba41a036a04
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-07-09 17:43:59 +0530

    expo image picker

--- client/components/StoriesBar.tsx
+++ client/components/StoriesBar.tsx
@@ -1,8 +1,11 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { styles } from '@/assets/styles/StoriesBar.styles';
 import { UserStory } from '@/types';
 import { dummyStoriesData } from '@/assets/assets';
+import { Ionicons } from '@expo/vector-icons';
+import { Colors } from '@/constants/Colors';
+import * as ImagePicker from 'expo-image-picker'
 
 interface StoriesBarProps{
     onViewStory: (us:UserStory)=> void
@@ -14,9 +17,33 @@ export default function StoriesBar({onViewStory}: StoriesBarProps) {
 
     const [uploading, setUploading]= useState(false)
     const {userStories} = {userStories: dummyStoriesData}
-    
+
+    const pickAndUpload = async () => {
+        const {status} = await ImagePic
+        
+    }
+
   return (
-    <FlatList horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.container} data={[]}>
+    <FlatList 
+    horizontal 
+    showsHorizontalScrollIndicator={false} 
+    contentContainerStyle={styles.container} 
+    data={[{_addStory: true}, ...userStories] as any[]}
+    keyExtractor={(item, i)=>(item._addStory ? "add" : item.user._id || String(i))}
+    renderItem={({item})=>{
+        if (item._addStory) {
+            return(
+                <TouchableOpacity style={styles.storyItem} onPress={pickAndUpload} disabled={uploading}>
+                    <View style={styles.addCircle}>
+                        <Ionicons name={uploading ? "hourglass" : "add"} size={24} color={Colors.onSurfaceVariant}/>
+                    </View>
+                    <Text style={styles.label}>Your Story</Text>
+
+                </TouchableOpacity>
+            )
+            
+        }
+    }}>
 
     </FlatList>
   )
