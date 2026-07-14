2d30b0d0f0c92ff2ebcd8477ac35b5383943efe0
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-07-14 14:31:02 +0530

    commit

--- client/components/ConvoItem.tsx
+++ client/components/ConvoItem.tsx
@@ -1,6 +1,7 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Conversation } from '@/types';
+import { styles } from '@/assets/styles/ConvoItem.styles';
 
 interface ConvoItemProps{
     convo: Conversation,
@@ -13,13 +14,15 @@ export default function ConvoItem({convo, selected, onPress} :  ConvoItemProps) 
     const name = convo.participant?.name || "User";
     const avatar = convo.participant?.avatar;
     const online = convo.participant?.isOnline;
-    const sub = `@${convo.participant?.handle}`
+    const sub = `@${convo.participant?.handle}`;
+    const lastMsg = convo.lastMessage?.text || (convo.lastMessage?.mediaType === "image" ? "Photo" : convo.lastMessage?.mediaUrl ? "Video" : "Start a Conversation")
+
 
 
 
   return (
-    <View>
-      <Text>ConvoItem</Text>
-    </View>
+    <TouchableOpacity activeOpacity={0.7} onPress={onPress} style={[styles]}>
+
+    </TouchableOpacity>
   )
 }
