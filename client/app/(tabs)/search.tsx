dc5328e958e5876c90a4fb5ecd96677a55ffbcb6
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-07-20 21:39:17 +0530

    search tsx

--- client/app/(tabs)/search.tsx
+++ client/app/(tabs)/search.tsx
@@ -1,7 +1,31 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import type {User as IUser} from '../../types'
+import { useRouter } from 'expo-router';
+import { dummyUsers } from '@/assets/assets';
 
 export default function search() {
+
+  const [search, setSearch]=useState("")
+  const [users, setUsers]=useState<IUser[]>([])
+  const [loading, setLoading]=useState(false)
+  const router = useRouter()
+
+  const fetchUsers = async () => {
+    setLoading(true)
+    setTimeout(()=>{
+      setUsers(dummyUsers)
+      setLoading(false)
+    },1000)
+    
+  }
+
+  useEffect(()=>{
+    const timer = setTimeout(fetchUsers, 300)
+    return ()=> clearTimeout(timer)
+  },[search])
+
+
   return (
     <View>
       <Text>search</Text>
