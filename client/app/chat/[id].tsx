effc5e36a6184c76d793ef11fbd229754bc3bc03
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-07-26 11:29:49 +0530

    is kotlin better?

--- client/app/chat/[id].tsx
+++ client/app/chat/[id].tsx
@@ -1,7 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { useRouter } from 'expo-router';
+import { dummyConversationData, dummyMessages, dummyUserProfile, dummyUsers } from '@/assets/assets';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from '@/assets/styles/ChatScreen.styles';
+import { Ionicons } from '@expo/vector-icons';
+import { Colors } from '@/constants/Colors';
+
 
 export default function ChatScreen() {
+
+  const router = useRouter()
+  let {auth, messages, users, selectedConversation, typingUsers}= {
+    auth: {user: dummyUserProfile},
+    messages:dummyMessages,
+    users: dummyUsers,
+    selectedConversation: dummyConversationData[0],
+    typingUsers:{
+      [dummyUsers[0]._id]: true,
+    }
+  }
+
+  const [text, setText]=useState("")
+  const [sending, setSending]=useState(false)
+  const [loading, setLoading]=useState(false)
+  const [mediaUri, setMediaUri]=useState<string | null>(null)
+
+  if (!selectedConversation) {
+    return (
+      <SafeAreaView style={styles.safe} >
+        <TouchableOpacity style={styles.backBtn} onPress={()=>router.back()}>
+          <Ionicons name="chevron-back" size={24} color={Colors.onSurface}/>
+        </TouchableOpacity>
+        <View style={styles.emptyState}>
+
+        </View>
+
+      </SafeAreaView>
+    )
+    
+  }
+
+
+
+
   return (
     <View>
       <Text>chat id</Text>
