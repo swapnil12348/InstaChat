109bc2c154e03dc1d8130135f254f8ebc19646f0
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-08-03 13:34:34 +0530

    corrections mannn

--- client/components/Bubble.tsx
+++ client/components/Bubble.tsx
@@ -8,64 +8,63 @@ import { styles } from '@/assets/styles/Bubble.styles';
 import { useVideoPlayer, VideoView } from 'expo-video';
 
 
-interface BubbleProps{
+interface BubbleProps {
     msg: Message;
     isMine: boolean;
 
 }
 
-export default function Bubble({msg, isMine} : BubbleProps) {
+export default function Bubble({ msg, isMine }: BubbleProps) {
 
-    const content = <BubbleContent msg={msg} isMine={isMine}/>
-
-
-  return (
-
-    <View style={[styles.row, isMine ? styles.rowMe : styles.rowThem]}>
-        {isMine? (
-            <LinearGradient
-            colors={[Colors.primary, Colors.primaryContainer]}
-            start={{x:0, y:0}}
-            end={{x:1, y:1}}
-            style={[styles.bubble, styles.bubble]}>
-                {content}
-
-            </LinearGradient>
-        ):(
-            <View style={[styles.bubble, styles.bubbleThem]}>{content}</View>
-        )}
-      
-    </View>
-  )
-}
-
-function BubbleContent({msg, isMine}: {msg: Message; isMine: boolean}){
-    return(
-        <View>
-            {msg.mediaUrl && (
-                <View style={styles.mediaWrapper}>
-                    {msg.mediaType === 'image' ? (
-                        <TouchableOpacity onPress={()=>Linking.openURL(msg.mediaUrl!)}>
-                        <Image source={{uri: msg.mediaUrl}} style={styles.mediaImage} resizeMode='cover'/>
-                    </TouchableOpacity>
+    const content = <BubbleContent msg={msg} isMine={isMine} />
 
 
-                    ):(
-                        <VideoPlayer uri={msg.mediaUrl} style={styles.mediaVideo}/>
+    return (
 
-                    )}
-                    
-                </View>
+        <View style={[styles.row, isMine ? styles.rowMe : styles.rowThem]}>
+            {isMine ? (
+                <LinearGradient
+                    colors={[Colors.primary, Colors.primaryContainer]}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 1 }}
+                    style={[styles.bubble, styles.bubbleMe]}>
+                    {content}
+
+                </LinearGradient>
+            ) : (
+                <View style={[styles.bubble, styles.bubbleThem]}>{content}</View>
             )}
-            {msg.text && <Text style={[styles.msgText, isMine? styles.msgTextMe : styles.msgTextThem]}>
-            </Text>}
+
         </View>
     )
 }
 
-function VideoPlayer({uri, style}:{uri: string, style: any}){
-    const player = useVideoPlayer({uri}, (p)=>{
-        p.loop=false
+function BubbleContent({ msg, isMine }: { msg: Message; isMine: boolean }) {
+    return (
+        <View>
+            {msg.mediaUrl && (
+                <View style={styles.mediaWrapper}>
+                    {msg.mediaType === 'image' ? (
+                        <TouchableOpacity onPress={() => Linking.openURL(msg.mediaUrl!)}>
+                            <Image source={{ uri: msg.mediaUrl }} style={styles.mediaImage} resizeMode='cover' />
+                        </TouchableOpacity>
+
+
+                    ) : (
+                        <VideoPlayer uri={msg.mediaUrl} style={styles.mediaVideo} />
+
+                    )}
+                </View>
+            )}
+
+            {msg.text && <Text style={[styles.msgText, isMine ? styles.msgTextMe : styles.msgTextThem]}>{msg.text}</Text>}
+        </View>
+    )
+}
+
+function VideoPlayer({ uri, style }: { uri: string, style: any }) {
+    const player = useVideoPlayer({ uri }, (p) => {
+        p.loop = false
     })
-    return <VideoView player={player} style={style} nativeControls/>
+    return <VideoView player={player} style={style} nativeControls />
 }
