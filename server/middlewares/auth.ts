4611c34be036cf342a64433ea8d6659531f1ade1
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-08-04 22:36:08 +0530

    server.ts

---
+++ server/middlewares/auth.ts
@@ -0,0 +1,22 @@
+import {Request, Response, NextFunction} from "express"
+import{clerkMiddleware, clerkClient, requireAuth, getAuth} from'@clerk/express'
+import User from "../models/User";
+export interface AuthRequest extends Request{
+    user?: {id: string, name: string, email: string}
+}
+export const authMiddleware = async (req: AuthRequest, res: Response, next: NextFunction) => {
+    try {
+        const {userId} = getAuth(req)
+        if (!userId) {
+            res.status(401).json({success: false, message: "Unauthenticated"})
+        }
+
+        //check if user exists locally on Mongodb
+
+        let localUser= await User.findById(userId)
+        
+    } catch (error) {
+        
+    }
+    
+}
--- server/server.ts
+++ server/server.ts
@@ -13,6 +13,7 @@ connectDB()
 // Middleware
 app.use(cors())
 app.use(express.json());
+app.use(clerkMiddleware())
 
 const port = process.env.PORT || 3000;
 
