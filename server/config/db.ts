4623aa35362c0d4850ae3bde317dfc2df11a423e
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-08-04 17:47:27 +0530

    basic server

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import console from "console";
 
 const connectDB = async ()=>{
-    mongoose.connection.on('connected', async ()=> console.log("mongoDb connected"));
-    if (!process.env.MONDODB_URI) throw new Error("Mongodb uri is not defined")
+    mongoose.connection.on('connected', async ()=> console.log("mongoDB is connected"));
 
+    if (!process.env.MONGODB_URI) throw new Error("Mongodb uri is not defined")
+        await mongoose.connect(process.env.MONGODB_URI);
 
 }
+
+export default connectDB
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,13 @@
 import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 const app = express();
+
+//connect to Mongodb
+
+connectDB()
 
 // Middleware
 app.use(cors())
