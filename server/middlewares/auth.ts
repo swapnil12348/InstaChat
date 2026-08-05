3281b6f6d46c78874ab48f2c082ccef6c019bfa3
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-08-05 18:32:38 +0530

    authorization

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -1,9 +1,14 @@
 import {Request, Response, NextFunction} from "express"
 import{clerkMiddleware, clerkClient, requireAuth, getAuth} from'@clerk/express'
 import User from "../models/User";
+
+
+
 export interface AuthRequest extends Request{
     user?: {id: string, name: string, email: string}
 }
+
+
 export const authMiddleware = async (req: AuthRequest, res: Response, next: NextFunction) => {
     try {
         const {userId} = getAuth(req)
@@ -14,6 +19,11 @@ export const authMiddleware = async (req: AuthRequest, res: Response, next: Next
         //check if user exists locally on Mongodb
 
         let localUser= await User.findById(userId)
+
+        if (!localUser) {
+            // lazy sync: fetch details from clerk api
+            
+        }
         
     } catch (error) {
         
