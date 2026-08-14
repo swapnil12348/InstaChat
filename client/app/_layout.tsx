5ed4b6da03bec4212c1449ac50f0bf900283a184
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-08-14 21:11:40 +0530

    react and native expo

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,14 +1,28 @@
-import { Redirect, Stack } from "expo-router";
+import { AppProvider } from "@/context/AppContext";
+import { Redirect, SplashScreen, Stack, useSegments } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { ClerkLoaded, ClerkProvider, useAuth } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 
 
 
 function AuthGaurd(){
-  const {isSignedIn} = {isSignedIn: true}
+  const {isSignedIn, isLoaded} = useAuth()
+  const segments= useSegments();
 
-  if (!isSignedIn) {
+  SplashScreen.hideAsync();
+  const inAuth = segments[0] === "(auth)"
+  
+
+  if (!isSignedIn && !inAuth) {
     return <Redirect href="/(auth)"/>
     
   }else if(isSignedIn){
@@ -19,9 +33,13 @@ function AuthGaurd(){
 
 export default function RootLayout() {
   
-  return<GestureHandlerRootView style={{flex: 1}}>
+  return<ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+    <ClerkLoaded>
 
-    <AuthGaurd/>
+    
+    <GestureHandlerRootView style={{flex: 1}}>
+    <AppProvider>
+      <AuthGaurd/>
 
       <Stack screenOptions={{headerShown: false}}>
         <Stack.Screen name="(auth)"/>
@@ -31,7 +49,17 @@ export default function RootLayout() {
       </Stack>
       <StatusBar style="dark"/>
 
+    </AppProvider>
+
+    
+
   </GestureHandlerRootView>
+  </ClerkLoaded>
   
 
+
+
+  </ClerkProvider>
+  
+  
 }
