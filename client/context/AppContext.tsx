84b3335916ef3a86cf5783ee0a68cac1029a18cf
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2026-08-14 20:59:05 +0530

    app context

--- client/context/AppContext.tsx
+++ client/context/AppContext.tsx
@@ -1,10 +1,38 @@
 import { AuthState, User } from "@/types";
-import { createContext } from "react";
+import { createContext, ReactNode, useCallback, useContext, useState } from "react";
 
 
 interface AppContextType{
     auth:AuthState;
-    login: (token: string, user: User)=> Promise<void>
+   
+    logout: ()=>Promise<void>;
+    updateUser: (user:User)=>Promise<void>;
 }
 
 const AppContext = createContext<AppContextType | null>(null)
+
+export function AppProvider({children}:{children:ReactNode}){
+
+    const [auth,setAuth] = useState<AuthState>({token:null, user:null, loading:true})
+    const [users,setUsers]= useState<User[]>([])
+    const logout = useCallback(async ()=>{
+
+    },[])
+
+    const updateUser = useCallback(async () => {
+        
+    },[])
+
+    return(
+        <AppContext.Provider value={{auth,logout,updateUser}}>
+            {children}
+        </AppContext.Provider>
+    )
+
+}
+
+export function useApp(){
+    const ctx = useContext(AppContext)
+    if(!ctx) throw new Error("useApp must be used inside AppProvider")
+        return ctx;
+}
